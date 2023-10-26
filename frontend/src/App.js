f5289f6da29d6a637f176d5e10926c435f9c2eea
Sundar <sundar10072001@gmail.com>
2023-10-26 16:34:43 +0530

    sundar changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Route } from "react-router-dom";
 import Candidate from './Component/Candidate';
+import Login from './Component/LoginNew/Login';
 
 function App() {
   return (
     <div>
-      <Candidate/>
+      {/* <Candidate/> */}
+      <Login/>
     </div>
   )
 }
