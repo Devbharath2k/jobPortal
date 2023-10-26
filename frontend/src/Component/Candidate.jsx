e06b17d22c019e2eb5bbc9e42047c3fe6f50b1e9
nivetha <135101423+nivethaaxess@users.noreply.github.com>
2023-10-26 12:54:46 +0530

    add

---
+++ frontend/src/Component/Candidate.jsx
@@ -0,0 +1,144 @@
+import React from "react";
+import "./Candidate.css";
+import logo from "../assets/IT.png";
+// import { Link } from 'react-router-dom';
+
+
+function Candidate() {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            <img src={logo} alt="" height={"49px"} />
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                {/* <Link className="nav-link active" aria-current="page" to="#">
+                  Home
+                </Link> */}
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" to="#">
+                  Link
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <div className="container-fluid">
+        <div class="section">
+          <div class="container">
+            <div class="row full-height justify-content-center">
+              <div class="col-12 text-center align-self-center py-5">
+                <div class="section pb-5 pt-5 pt-sm-2 text-center">
+                  <h6 class="mb-0 pb-3">
+                    <span className="text-white">Candidate</span>
+                    <span className="text-white">Employer</span>
+                  </h6>
+                  <input
+                    class="checkbox"
+                    type="checkbox"
+                    id="reg-log"
+                    name="reg-log"
+                  />
+                  <label for="reg-log" />
+                  <div class="card-3d-wrap mx-auto">
+                    <div class="card-3d-wrapper">
+                      <div class="card-front">
+                        <div class="center-wrap">
+                          <div class="section text-center">
+                            <h4 class="mb-4 pb-3 mt-5 text-white"> Candidate Log In</h4>
+                            <div class="form-group">
+                              <input
+                                type="email"
+                                name="logemail"
+                                class="form-style mb-2"
+                                placeholder="Your Email"
+                                id="logemail"
+                                autocomplete="off"
+                              />
+                              <i class="input-icon uil uil-at" />
+                            </div>
+                            <div class="form-group mt-2">
+                              <input
+                                type="password"
+                                name="logpass"
+                                class="form-style"
+                                placeholder="Your Password"
+                                id="logpass"
+                                autocomplete="off"
+                              />
+                              <i class="input-icon uil uil-lock-alt" />
+                            </div>
+                           <button className="btn btn-primary mt-3">Submit</button>
+                            <p class="mb-0 mt-4 text-center">
+                              {/* <Link to="#" class="link">
+                                New user <span className=""> Candidate Sign Up</span>
+                              </Link> */}
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="card-back">
+                        <div class="center-wrap">
+                          <div class="section text-center">
+                            <h4 class="mb-4 pb-3 mt-4 text-white">Employer Login </h4>
+
+                            <div class="form-group">
+                              <input
+                                type="email"
+                                name="logemail"
+                                class="form-style mb-2"
+                                placeholder="Your Email"
+                                id="logemail"
+                                autocomplete="off"
+                              />
+                              <i class="input-icon uil uil-at" />
+                            </div>
+                            <div class="form-group mt-2">
+                              <input
+                                type="password"
+                                name="logpass"
+                                class="form-style"
+                                placeholder="Your Password"
+                                id="logpass"
+                                autocomplete="off"
+                              />
+                              <i class="input-icon uil uil-lock-alt" />
+                            </div>
+                           <button className="btn btn-primary mt-4">Submit</button>
+                            <p class="mb-0 mt-4 text-center">
+                              <a to="#" class="link">
+                                New user <span className="">Employer Sign Up</span>
+                              </a>
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Candidate;
---
+++ frontend/src/Component/Employer_profile.js
@@ -0,0 +1,223 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+
+const Employer_profile = () => {
+  return (
+    <div id="employer_profile" className="employer_profile pt-5">
+      <div className="container-fluid">
+        <p className="text-center justify-content-center display-6">
+          Employer Profile
+        </p>
+        <div className="row my-5 justify-content-center">
+            
+          <div className=" col-md-10 card">
+            <div className="card-body text-center">
+                <div className="row">
+                    <div className="col">
+                    <label for="exampleFormControlInput1" className="form-label">
+                    Role Name
+                  </label>
+                    </div>
+                    <div className="col">
+                    <label for="exampleFormControlInput1" className="form-label">
+                    Number of openings
+                  </label>
+                    </div>
+                    <div className="col">
+                    <label for="exampleFormControlInput1" className="form-label">
+                    Budget
+                  </label>
+                    </div>
+                    <div className="col">
+                    <label for="exampleFormControlInput1" className="form-label">
+                    Experiance
+                  </label>
+                    </div>
+                    <div className="col">
+                    <label for="exampleFormControlInput1" className="form-label">
+                    Availability
+                  </label>
+                    </div>
+                    <div className="col">
+                    <label for="exampleFormControlInput1" className="form-label">
+                    Location
+                  </label>
+                    </div>
+                </div>
+              <div className="row justify-content-center">
+                <div className="col">
+                  
+                  <input
+                    type="text"
+                    name="Role Name"
+                    className="form-control"
+                    placeholder="Role Name"
+                    id=""
+                  />
+                </div>
+                <div className="col">
+                  
+
+                  <input
+                    type="number"
+                    name="Number of openings"
+                    className="form-control"
+                    placeholder="1 "
+                    id=""
+                  />
+                </div>
+                <div className="col">
+                  
+
+                  <input
+                    type="number"
+                    name="Budget"
+                    className="form-control"
+                    placeholder="CTC"
+                    id=""
+                  />
+                </div>
+                <div className="col">
+                  
+
+                  <input
+                    type="number"
+                    name="Budget"
+                    className="form-control"
+                    placeholder="YRS"
+                    id=""
+                  />
+                </div>
+
+                <div className="col-md-2">
+                  
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="DAYS"
+                    name=""
+                  />
+                </div>
+                <div className="col-md-2">
+                  
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="location"
+                    name=""
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-2 card"></div>
+        </div>
+      </div>
+    </div>
+  );
+
+  {
+    /* <div className="row justify-content-center mt-3">
+                        <div className="col">
+                            <label for="exampleFormControlInput1" className="form-label">Candidate Name</label>
+                            <input type="text" name="fname" className="form-control" placeholder="krishna" id=""/>
+                        </div>
+                        <div className="col">
+                            <label for="exampleFormControlInput1" className="form-label">Role</label>
+                            <select className="form-select" aria-label="Default select example">
+                                <option selected>Testing</option>
+                                <option value="1">2year</option>
+                                <option value="2">3year</option>
+                                <option value="3">5+year</option>
+                            </select>
+                        </div>
+                        <div className="col">
+                            <label for="exampleFormControlInput1" className="form-label">Date</label>
+                            <input type="datetime-local" name="date" className="form-control" placeholder="Budget" id=""/>
+                        </div>
+                        <div className="col">
+                            <label for="exampleFormControlInput1" className="form-label">Status</label>
+                            <input type="text" name="closed" placeholder="L1 Completed" id=""/>
+                        </div>
+                        <div className="col-md-2">
+                            <label for="exampleFormControlInput1" className="form-label">command</label>
+                            <input type="text" className="form-control" placeholder="Review" name="Experiance"/>
+                        </div>
+                        <div>
+                            <button className="btn btn-primary mt-3">Submit</button>
+                        </div>
+                    </div> */
+  }
+
+  {
+    /* <div className="col-md-2">
+                            <label for="exampleFormControlInput1" className="form-label">Availability</label>
+                            <select className="form-select" aria-label="Default select example">
+                                <option selected>immediate</option>
+                                <option value="1">Banglore</option>
+                                <option value="2">Hyderabad</option>
+                                <option value="3">Triuvandaran</option>
+                            </select>
+                        </div>
+                        <div className="col-md-2">
+                            <label for="exampleFormControlInput1" className="form-label">Location</label>
+                            <select className="form-select" aria-label="Default select example">
+                                <option selected>chennai</option>
+                                <option value="1">Banglore</option>
+                                <option value="2">Hyderabad</option>
+                                <option value="3">Triuvandaran</option>
+                            </select>
+                        </div> */
+  }
+  {
+    /* <div className="row justify-content-center mt-3">
+                        <div className="col-md-2">
+                            <label for="exampleFormControlInput1" className="form-label">Name of roles</label>
+                            <select className="form-select" aria-label="Default select example">
+                                <option selected>Devops</option>
+                                <option value="1">2</option>
+                                <option value="2">3</option>
+                                <option value="3">Devops</option>
+                            </select>
+                        </div>
+                        <div className="col-md-2">
+                            <label for="exampleFormControlInput1" className="form-label">No of openings </label>
+                            <select className="form-select" aria-label="Default select example">
+                                <option selected>3 </option>
+                                <option value="1">2year</option>
+                                <option value="2">3year</option>
+                                <option value="3">5+year</option>
+                            </select>
+                        </div>
+                        <div className="col-md-2">
+                            <label for="exampleFormControlInput1" className="form-label">Budget</label>
+                            <input type="number" name="number" className="form-control" placeholder="Budget" id=""/>
+                        </div>
+                        <div className="col-md-2">
+                            <label for="exampleFormControlInput1" className="form-label">Experiance</label>
+                            <input type="number" className="form-control" placeholder="3+" name="Experiance"/>
+                        </div>
+
+                        <div className="col-md-2">
+                            <label for="exampleFormControlInput1" className="form-label">Availability</label>
+                            <select className="form-select" aria-label="Default select example">
+                                <option selected>immediate</option>
+                                <option value="1">Banglore</option>
+                                <option value="2">Hyderabad</option>
+                                <option value="3">Triuvandaran</option>
+                            </select>
+                        </div>
+                        <div className="col-md-2">
+                            <label for="exampleFormControlInput1" className="form-label">Location</label>
+                            <select className="form-select" aria-label="Default select example">
+                                <option selected>chennai</option>
+                                <option value="1">Banglore</option>
+                                <option value="2">Hyderabad</option>
+                                <option value="3">Triuvandaran</option>
+                            </select>
+                        </div> 
+                </div> */
+  }
+};
+
+export default Employer_profile;
