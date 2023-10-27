a22aff2f66779f17ce9cf98d0bb84ca1a3661edb
nivetha <135101423+nivethaaxess@users.noreply.github.com>
2023-10-27 10:20:42 +0530

    add

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Candidate from './Component/Candidate';
 import Employer_Status from './Component/Employer_status';
 import Employer_profile from './Component/Employer_profile';
 import Candidate_profile from './Component/Candidate_profile';
+import CandidateUpdate from "./Component/Candidate_profile"
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path='/Candidate_profile' element={<Candidate_profile/>}/>
         <Route path='/employer_Status' element={<Employer_Status/>}/>
         <Route path='/employer_profile' element={<Employer_profile/>}/>
+        <Route path ="./Candidate_profile" element ={<CandidateUpdate/>}/>
     </Routes>
 
 
--- frontend/src/Component/Employer_profile.jsx
+++ frontend/src/Component/Employer_profile.jsx
@@ -1,218 +1,175 @@
-import React from "react";
-// import { Link } from "react-router-dom";
- import "../Style/employer_profile.css"
+import React, { useState } from "react";
+import "../Style/employer_profile.css"
 
 const Employer_profile = () => {
+  const [roleCount, setRoleCount] = useState(1);
+  const [formData, setFormData] = useState([{ roleName: "", openings: 0, budget: 0, experience: 0, days: "", location: "" }]);
+  const [emptyFields, setEmptyFields] = useState(Array.from({ length: formData.length }, () => ({})));
+
+  const handleInputChange = (index, e) => {
+    const { name, value } = e.target;
+    const newFormData = [...formData];
+    newFormData[index][name] = value;
+    setFormData(newFormData);
+  };
+
+  const handleDelete = (index) => {
+    const newFormData = [...formData];
+    newFormData.splice(index, 1);
+    setFormData(newFormData);
+  };
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  
+  //   // Validation: Check if any form field is empty
+  //   const hasEmptyFields = formData.some((role) => Object.values(role).some((value) => value === ''));
+  
+  //   if (hasEmptyFields) {
+  //     // Handle empty fields (show an error message, prevent form submission, etc.)
+  //     console.error("Error: All fields must be filled out.");
+  //      // Highlight empty fields by adding a class
+  //   const emptyFields = formData.map((role) => Object.fromEntries(Object.entries(role).map(([key, value]) => [key, value === ''])));
+  //   setEmptyFields(emptyFields);
+  //   } else {
+  //     // All fields are filled, proceed with form submission or API call
+  //     // Send formData to your API or perform necessary actions here
+  //     console.log(formData);
+  //   }
+  // };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  
+    // Validation: Check if any form field is empty
+    const emptyFieldsData = formData.map((role) => {
+      return Object.fromEntries(Object.entries(role).map(([key, value]) => [key, value === '']));
+    });
+  
+    setEmptyFields(emptyFieldsData);
+  
+    const hasEmptyFields = emptyFieldsData.some((role) => Object.values(role).some((value) => value));
+  
+    if (hasEmptyFields) {
+      // Handle empty fields (show an error message, prevent form submission, etc.)
+      console.error("Error: All fields must be filled out.");
+    } else {
+      // No empty fields, proceed with form submission or API call
+      // Send formData to your API or perform necessary actions here
+      console.log(formData);
+    }
+  };
+  
+  
+
+  const addRole = () => {
+    setRoleCount(roleCount + 1);
+    setFormData([...formData, { roleName: "", openings: 0, budget: 0, experience: 0, days: "", location: "" }]);
+    setEmptyFields([...emptyFields, {}]); 
+  };
+
   return (
-    <div id="employer_profile" className="employer_profile pt-5">
-      <div className="container-fluid">
-        <p className="text-center justify-content-center display-6">
-          Employer Profile
-        </p>
-        <div className="row my-5 justify-content-center">
-          <div className=" col-md-10 card">
-            <div className="card-body text-center">
-              <div className="row">
-                <div className="col">
-                  <label for="exampleFormControlInput1" className="form-label">
-                    Role Name
-                  </label>
-                </div>
-                <div className="col">
-                  <label for="exampleFormControlInput1" className="form-label">
-                    Number of openings
-                  </label>
-                </div>
-                <div className="col">
-                  <label for="exampleFormControlInput1" className="form-label">
-                    Budget
-                  </label>
-                </div>
-                <div className="col">
-                  <label for="exampleFormControlInput1" className="form-label">
-                    Experiance
-                  </label>
-                </div>
-                <div className="col">
-                  <label for="exampleFormControlInput1" className="form-label">
-                    Availability
-                  </label>
-                </div>
-                <div className="col">
-                  <label for="exampleFormControlInput1" className="form-label">
-                    Location
-                  </label>
-                </div>
-              </div>
-              <div className="row justify-content-center">
-                <div className="col">
+    <div id="employer_profile" className="employer_profile py-5  text-center">
+      <h2 className="py-5">Employer_profile</h2>
+      <div className="row py-3 labels align-items-center justify-content-center">
+            
+            <div className="col">Role Name</div>
+            <div className="col"> Openings</div>
+            <div className="col">Budget</div>
+            <div className="col">Experience</div>
+            <div className="col">Days</div>
+            <div className="col">Location</div>
+            <div className="col bg-body-secondary">
+            <i class="bi bi-plus-circle-fill plus-icon" onClick={addRole}></i>
+            </div>
+      </div>
+      {/* ...your existing code... */}
+      <form onSubmit={handleSubmit} >
+            {/* ... your input fields ... */}
+            {formData.map((role, index) => (
+             <div key={index} className={`row justify-content-center job pt-3 ${emptyFields[index].roleName ? 'has-error' : ''}`}>
+    
+             <div className="col">
                   <input
                     type="text"
-                    name="Role Name"
-                    className="form-control"
+                    name="roleName"
+                    className={`form-control `}
+        
                     placeholder="Role Name"
-                    id=""
+                    value={role.roleName}
+                    onChange={(e) => handleInputChange(index, e)}
+                    required
                   />
                 </div>
                 <div className="col">
                   <input
                     type="number"
-                    name="Number of openings"
+                    name="openings"
                     className="form-control"
-                    min={"0"}
-                    placeholder="1 "
-                    id=""
+                    min={0}
+                    placeholder="Number of Openings"
+                    value={role.openings}
+                    onChange={(e) => handleInputChange(index, e)}
+                    required
                   />
                 </div>
                 <div className="col">
                   <input
                     type="number"
-                    name="Budget"
+                    name="budget"
                     className="form-control"
-                    placeholder="CTC"
-                    min={"0"}
-                    id=""
+                    min={0}
+                    placeholder="Budget"
+                    value={role.budget}
+                    onChange={(e) => handleInputChange(index, e)}
+                    required
                   />
                 </div>
                 <div className="col">
                   <input
                     type="number"
-                    name="Budget"
+                    name="experience"
                     className="form-control"
-                    placeholder="YRS"
-                    min={"0"}
-                    id=""
+                    min={0}
+                    placeholder="Experience"
+                    value={role.experience}
+                    onChange={(e) => handleInputChange(index, e)}
+                    required
                   />
                 </div>
-
                 <div className="col-md-2">
                   <input
                     type="text"
                     className="form-control"
-                    placeholder="DAYS"
-                    name=""
+                    placeholder="Days"
+                    name="days"
+                    value={role.days}
+                    onChange={(e) => handleInputChange(index, e)}
+                    required
                   />
                 </div>
-                <div className="col-md-2 d-flex">
+                <div className="col d-flex">
                   <input
                     type="text"
                     className="form-control"
-                    placeholder="location"
-                    name=""
+                    placeholder="Location"
+                    name="location"
+                    value={role.location}
+                    onChange={(e) => handleInputChange(index, e)}
+                    required
                   />
-                  <span className="ms-2 "><i class="bi bi-trash-fill"></i></span>
+                </div>
+                <div className="col">
+                  <span className="ms-2 text-danger" onClick={() => handleDelete(index)}>
+                    <i className="bi bi-trash-fill delete-icon" style={{ fontSize: "20px" }}></i>
+                  </span>
                 </div>
               </div>
-            </div>
-          </div>
-          <div className="col-md-2 card"></div>
-        </div>
-      </div>
+            ))}
+        <button type="submit" className="p-1 my-5 bg-primary rounded-3 submit-button">Submit</button>
+      </form>
     </div>
   );
-
-  {
-    /* <div className="row justify-content-center mt-3">
-                        <div className="col">
-                            <label for="exampleFormControlInput1" className="form-label">Candidate Name</label>
-                            <input type="text" name="fname" className="form-control" placeholder="krishna" id=""/>
-                        </div>
-                        <div className="col">
-                            <label for="exampleFormControlInput1" className="form-label">Role</label>
-                            <select className="form-select" aria-label="Default select example">
-                                <option selected>Testing</option>
-                                <option value="1">2year</option>
-                                <option value="2">3year</option>
-                                <option value="3">5+year</option>
-                            </select>
-                        </div>
-                        <div className="col">
-                            <label for="exampleFormControlInput1" className="form-label">Date</label>
-                            <input type="datetime-local" name="date" className="form-control" placeholder="Budget" id=""/>
-                        </div>
-                        <div className="col">
-                            <label for="exampleFormControlInput1" className="form-label">Status</label>
-                            <input type="text" name="closed" placeholder="L1 Completed" id=""/>
-                        </div>
-                        <div className="col-md-2">
-                            <label for="exampleFormControlInput1" className="form-label">command</label>
-                            <input type="text" className="form-control" placeholder="Review" name="Experiance"/>
-                        </div>
-                        <div>
-                            <button className="btn btn-primary mt-3">Submit</button>
-                        </div>
-                    </div> */
-  }
-
-  {
-    /* <div className="col-md-2">
-                            <label for="exampleFormControlInput1" className="form-label">Availability</label>
-                            <select className="form-select" aria-label="Default select example">
-                                <option selected>immediate</option>
-                                <option value="1">Banglore</option>
-                                <option value="2">Hyderabad</option>
-                                <option value="3">Triuvandaran</option>
-                            </select>
-                        </div>
-                        <div className="col-md-2">
-                            <label for="exampleFormControlInput1" className="form-label">Location</label>
-                            <select className="form-select" aria-label="Default select example">
-                                <option selected>chennai</option>
-                                <option value="1">Banglore</option>
-                                <option value="2">Hyderabad</option>
-                                <option value="3">Triuvandaran</option>
-                            </select>
-                        </div> */
-  }
-  {
-    /* <div className="row justify-content-center mt-3">
-                        <div className="col-md-2">
-                            <label for="exampleFormControlInput1" className="form-label">Name of roles</label>
-                            <select className="form-select" aria-label="Default select example">
-                                <option selected>Devops</option>
-                                <option value="1">2</option>
-                                <option value="2">3</option>
-                                <option value="3">Devops</option>
-                            </select>
-                        </div>
-                        <div className="col-md-2">
-                            <label for="exampleFormControlInput1" className="form-label">No of openings </label>
-                            <select className="form-select" aria-label="Default select example">
-                                <option selected>3 </option>
-                                <option value="1">2year</option>
-                                <option value="2">3year</option>
-                                <option value="3">5+year</option>
-                            </select>
-                        </div>
-                        <div className="col-md-2">
-                            <label for="exampleFormControlInput1" className="form-label">Budget</label>
-                            <input type="number" name="number" className="form-control" placeholder="Budget" id=""/>
-                        </div>
-                        <div className="col-md-2">
-                            <label for="exampleFormControlInput1" className="form-label">Experiance</label>
-                            <input type="number" className="form-control" placeholder="3+" name="Experiance"/>
-                        </div>
-
-                        <div className="col-md-2">
-                            <label for="exampleFormControlInput1" className="form-label">Availability</label>
-                            <select className="form-select" aria-label="Default select example">
-                                <option selected>immediate</option>
-                                <option value="1">Banglore</option>
-                                <option value="2">Hyderabad</option>
-                                <option value="3">Triuvandaran</option>
-                            </select>
-                        </div>
-                        <div className="col-md-2">
-                            <label for="exampleFormControlInput1" className="form-label">Location</label>
-                            <select className="form-select" aria-label="Default select example">
-                                <option selected>chennai</option>
-                                <option value="1">Banglore</option>
-                                <option value="2">Hyderabad</option>
-                                <option value="3">Triuvandaran</option>
-                            </select>
-                        </div> 
-                </div> */
-  }
 };
 
 export default Employer_profile;
