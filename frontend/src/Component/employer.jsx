5b121239b77c0b68760d471f032c24c73487cc7c
Sundar <sundar10072001@gmail.com>
2023-10-27 13:34:09 +0530

    new

--- frontend/src/Component/employer.jsx
+++ frontend/src/Component/employer.jsx
@@ -5,9 +5,12 @@ import { Link, Outlet } from 'react-router-dom'
 const Employer = () => {
   return (
     <div>
-        <div className='row'>
+        <div className='row g-0'>
             <div className='col-2'>
-                AD
+                <video autoPlay muted loop>
+                  <source src='https://player.vimeo.com/external/481048436.sd.mp4?s=c9afc848a048019e3a0cd6019737eb50133efe4f&profile_id=165&oauth2_token_id=57447761'
+                  ></source>
+                </video>
             </div>
             <div className='col-10'>
                
