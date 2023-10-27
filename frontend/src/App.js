36cbdc0508c1dd27b5deb06089ebe1d79684e37f
nivethaaxess <nivetha.rajendran@axesstechnology.in>
2023-10-27 13:03:32 +0530

    jay

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import { Route } from "react-router-dom";
-import Candidate from './Component/Candidate';
+// import { Route } from "react-router-dom";
+// import Candidate from './Component/Candidate';
+import  CandidateUpdate from '../src/Component/candidate_status';
 
 function App() {
   return (
     <div>
-      <Candidate/>
+      {/* <Candidate/> */}
+      <CandidateUpdate/>
     </div>
   )
 }
