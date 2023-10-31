85266d115457d3cf82226df5512ad4f96c8a61b2
devbharath2k <devbharath21k@gmail.com>
2023-10-31 16:50:20 +0530

    new

--- frontend/src/Component/Login/EmployerReg.jsx
+++ frontend/src/Component/Login/EmployerReg.jsx
@@ -1,73 +1,170 @@
-import React from 'react';
+import React from "react";
 import "./EmployerReg.css";
+import { useState } from "react";
+import axios from "axios";
 
 function EmployerReg() {
+  const [employers, setEmployers] = useState({
+    fname: "",
+    lname: "",
+    email: "",
+    company: "",
+    salary: "",
+    experience: "",
+    phone: "",
+    password: ""
+  });
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    axios.post("http://localhost:4000/employerpost", employers)
+      .then(response => {
+        console.log(response.data.data);
+      })
+      .catch(err => {
+        console.log(err);
+        console.log(err.message);
+      });
+  };
+
   return (
-    <div id='EmployerReg'>
-    <div className="container Register my-2 mt-3" id="EMployer">
-      <div className="text">Employer Register form</div>
-      <form action="#">
-        <div className="form-row">
-          <div className="input-data">
-            <input type="text" required />
-            <div className=""></div>
-            <label htmlFor="">First Name</label>
+    <div id="EmployerReg">
+      <div className="container Register my-2 mt-3" id="EMployer">
+        <div className="text">Employer Register form</div>
+        <form onSubmit={handleSubmit}>
+          <div className="form-row">
+            <div className="input-data">
+              <input
+                type="text"
+                value={employers.fname}
+                onChange={e =>
+                  setEmployers({
+                    ...employers,
+                    fname: e.target.value
+                  })
+                }
+              />
+              <div className="" />
+              <label htmlFor="">First Name</label>
+            </div>
+            <div className="input-data">
+              <input
+                type="text"
+                value={employers.lname}
+                onChange={e =>
+                  setEmployers({
+                    ...employers,
+                    lname: e.target.value
+                  })
+                }
+              />
+              <div className="underline" />
+              <label htmlFor="">Last Name</label>
+            </div>
           </div>
-          <div className="input-data">
-            <input type="text" required />
-            <div className="underline"></div>
-            <label htmlFor="">Last Name</label>
+          <div className="form-row">
+            <div className="input-data">
+              <input
+                type="email"
+                value={employers.email}
+                onChange={e =>
+                  setEmployers({
+                    ...employers,
+                    email: e.target.value
+                  })
+                }
+              />
+              <div className="underline" />
+              <label htmlFor="">Email Address</label>
+            </div>
+            <div className="input-data">
+              <input
+                type="text"
+                value={employers.company}
+                onChange={e =>
+                  setEmployers({
+                    ...employers,
+                    company: e.target.value
+                  })
+                }
+              />
+              <div className="underline" />
+              <label htmlFor="">Current company</label>
+            </div>
           </div>
-        </div>
-        <div className="form-row">
-          <div className="input-data">
-            <input type="email" required />
-            <div className="underline"></div>
-            <label htmlFor="">Email Address</label>
+          <div className="form-row">
+            <div className="input-data">
+              <input
+                type="text"
+                value={employers.salary}
+                onChange={e =>
+                  setEmployers({
+                    ...employers,
+                    salary: e.target.value
+                  })
+                }
+              />
+              <div className="underline" />
+              <label htmlFor="">Current CTC</label>
+            </div>
+            <div className="input-data">
+              <input
+                type="text"
+                value={employers.experience}
+                onChange={e =>
+                  setEmployers({
+                    ...employers,
+                    experience: e.target.value
+                  })
+                }
+              />
+              <div className="underline" />
+              <label htmlFor="">Total Experience</label>
+            </div>
           </div>
-          <div className="input-data">
-            <input type="text" required />
-            <div className="underline"></div>
-            <label htmlFor="">Current company</label>
+          <div className="form-row">
+            <div className="input-data">
+              <input
+                type="number"
+                value={employers.phone}
+                onChange={e =>
+                  setEmployers({
+                    ...employers,
+                    phone: e.target.value
+                  })
+                }
+              />
+              <div className="underline" />
+              <label htmlFor="">Phone Number</label>
+            </div>
+            <div className="input-data">
+              <input
+                type="password"
+                value={employers.password}
+                onChange={e =>
+                  setEmployers({
+                    ...employers,
+                    password: e.target.value
+                  })
+                }
+              />
+              <div className="underline" />
+              <label htmlFor="">Password</label>
+            </div>
           </div>
-        </div>
-        <div className="form-row">
-          <div className="input-data">
-            <input type="email" required />
-            <div className="underline"></div>
-            <label htmlFor="">Current CTC</label>
-          </div>
-          <div className="input-data">
-            <input type="text" required />
-            <div className="underline"></div>
-            <label htmlFor="">Total Experience</label>
-          </div>
-        </div>
-        <div className="form-row">
-          <div className="input-data">
-            <input type="number" required />
-            <div className="underline"></div>
-            <label htmlFor="">Phone Number</label>
-          </div>
-          <div className="input-data">
-            <input type="password" required />
-            <div className="underline"></div>
-            <label htmlFor="">Password</label>
-          </div>
-        </div>
-        <div className="form-row">
-          <div className="input-data textarea">
-            <div className="form-row submit-btn justify-content-center align-items-center">
-              <div className="input-data">
-                <div className="inner"></div>
-                <input type="submit" value="Submit" />
+          <div className="form-row">
+            <div className="input-data textarea">
+              <div className="form-row submit-btn justify-content-center align-items-center">
+                <div className="input-data">
+                  <div className="inner" />
+                  <input type="submit" value="Submit" />
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
-    </div> 
   );
 }
 
